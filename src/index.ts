e5ce7253303ccc4834dc752123b116d707d717dc
KTibow <KTibow@users.noreply.github.com>
2025-08-15 20:50:00 -0700

    actually generate map

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-export const todo = "indeed";
+import { hierarchy } from "./resources/map.ts";
+const getBestEncoding = (entities: string[]) => {
+  if (entities.length < 2) return entities[0];
+
+  let bestEntity = entities[0];
+  let bestEntityLength = bestEntity.length;
+  let bestEntityUppercases = [...bestEntity].filter(
+    (c) => c.toUpperCase() == c,
+  ).length;
+  for (let i = 1; i < entities.length; i++) {
+    const entity = entities[i];
+    const entityLength = entity.length;
+    const entityUppercases = [...entity].filter(
+      (c) => c.toUpperCase() == c,
+    ).length;
+    if (entityLength > bestEntityLength) continue;
+    if (
+      entityLength == bestEntityLength &&
+      entityUppercases >= bestEntityUppercases
+    )
+      continue;
+    bestEntity = entity;
+    bestEntityLength = entityLength;
+    bestEntityUppercases = entityUppercases;
+  }
+  return bestEntity;
+};
+export const encodeEntity = (text: string): string | undefined => {
+  if (text.length < 1) return undefined;
+
+  const c1 = text.charCodeAt(0);
+  const level1 = hierarchy.get(c1);
+  if (!level1) return undefined;
+
+  const c2 = text.length < 2 ? 0 : text.charCodeAt(1);
+  const level2 = level1.get(c2);
+  if (!level2) return undefined;
+  return getBestEncoding(level2);
+};
---
+++ src/lib/deltapacking.ts
@@ -0,0 +1,69 @@
+export type Unpacked = { index: number; value: string }[];
+export const pack = (
+  arr: Unpacked,
+  separator: string,
+  {
+    isEntity = false,
+    subseparator = "",
+  }: { isEntity?: boolean; subseparator?: string } = {},
+) => {
+  arr.sort((a, b) => a.index - b.index);
+  let output = "";
+  let arrIndex = 0;
+  let lastIndex = 0;
+  for (const { index, value } of arr) {
+    // Starting a new item requires a separator
+    if (arrIndex > 0) output += separator;
+    // and an increment
+    const assumedIncrement = arrIndex == 0 ? 0 : 1;
+    if (index - lastIndex != assumedIncrement) output += index - lastIndex;
+    // and sometimes a subseparator, to prevent the inner from being eaten
+    if (subseparator && "0123456789".includes(value[0])) {
+      output += subseparator;
+    }
+    // Now, add the value
+    if (isEntity) {
+      if (value.endsWith(";")) {
+        output += value.slice(1, -1);
+      } else {
+        output += value.slice(1) + "!";
+      }
+    } else {
+      output += value;
+    }
+
+    lastIndex = index;
+    arrIndex++;
+  }
+  return output;
+};
+export const unpack = (data: string, separator: string, isEntity = false) => {
+  const output: Unpacked = [];
+  let arrIndex = 0;
+  let index = 0;
+  for (let value of data.split(separator)) {
+    if (!value) continue;
+
+    const explicitIncrement = parseInt(value);
+    let increment: number;
+    if (Number.isNaN(explicitIncrement)) {
+      increment = arrIndex == 0 ? 0 : 1;
+    } else {
+      increment = explicitIncrement;
+      value = value.slice(increment.toString().length);
+    }
+    index += increment;
+
+    if (isEntity) {
+      if (value.endsWith("!")) {
+        value = `&${value.slice(0, -1)}`;
+      } else {
+        value = `&${value};`;
+      }
+    }
+
+    output.push({ index, value });
+    arrIndex++;
+  }
+  return output;
+};
